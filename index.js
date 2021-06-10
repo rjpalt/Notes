5b6c4f32f1f24926102606e916d6d0068c6e9434
Rasmus Paltschik <rjpalt@gmail.com>
2021-06-10 11:34:32 +0300

    Swap to phonebook

--- index.js
+++ index.js
@@ -1,89 +1,118 @@
 const express = require('express')
 const cors = require('cors')
+const morgan = require('morgan')
 
 const app = express();
-app.use(express.json());
+app.use(express.json())
+
+//Deactivated tiny version of morgan. Deactivate customized morgan to run tests on tiny.
+//app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+morgan.token('param', function(req, res, param) {
+    return req.params[param];
+});
 app.use(cors())
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2020-01-10T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2020-01-10T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2020-01-10T19:20:14.298Z",
-      important: true
+
+let persons = [
+  {
+   id: 1,
+   name: "Arto Hellas",
+   number: "040-123456",
+ },
+ {
+   id: 2,
+   name: "Ada Lovelace",
+   number: "39-44-5323523",
+ },
+ {
+   id: 3,
+   name: "Dan Abramov",
+   number: "12-43-234345",
+ },
+ {
+   id: 4,
+   name: "Mary Poppendieck",
+   number: "39-23-6423122",
+ },
+ {
+   id: 5,
+   name: "Rasmus Paltschik",
+   number: "040-5405646"
+ }
+]
+
+
+  //Send all resources
+  app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
+
+
+  //Get a single resource
+  app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+      res.json(person)
+    } else {
+      res.status(404).end()
     }
-  ]
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>')
-})
+  })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
-})
+  //Delete a resource
+  app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
 
-app.get('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const note = notes.find(note => note.id === id)
+    res.status(204).end()
+  })
 
-  if (note) {
-    res.json(note)
-  } else {
-    res.status(404).end()
+
+  const generateId = () => {
+
+    const id = Math.floor(Math.random() * 10000000)
+
+    return id
+    console.log(id)
+
   }
 
-})
+  //Add a resource
+  app.post('/api/persons', (req, res) => {
 
-app.delete('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  notes = notes.filter(note => note.id !== id)
+    const body = req.body
+    console.log(body)
 
-  res.status(204).end()
-})
-
-const generateId = () => {
-
-  const maxId = notes.length > 0
-  ? Math.max(...notes.map(n => n.id))
-  : 0
-
-  return maxId +1 ;
-
-}
-
-app.post('/api/notes', (req, res) => {
-
-  const body = req.body
-
-  if (!body.content) {
-    return res.status(400).json({
-      error: 'content missing'
+    if (!body.name) {
+      return res.status(400).json({
+        error: 'Name missing!'
     })
-  }
+    } else if (!body.number) {
+      return res.status(400).json({
+        error: 'Number missing!'
+      })
+    } else if (persons.find(person => person.name === body.name)) {
+        return res.status(400).json({
+            error: 'Name must be unique!'
+        })
+    }
 
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
+    const person = {
+      id: generateId(),
+      name: body.name,
+      number: body.number,
+    }
 
-  notes = notes.concat(note)
+    persons = persons.concat(person)
 
-  res.json(note)
-})
+    res.json(person)
+
+  })
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
